d30c06b29e844860f34a21c57ae4a3b56886916c
James Geneser <jamesgeneser@gmail.com>
2023-04-17 14:52:27 -0600

    changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,8 @@ require("dotenv").config({ path: "../.env" });
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  //   useCreateIndex: true,
+  //   useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
