322066153eea05a9baa6c82251ae3f306f36a97d
James Geneser <jamesgeneser@gmail.com>
2023-04-17 14:30:57 -0600

    removed _dirname

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-require("dotenv").config({ path: __dirname + "../.env" });
+require("dotenv").config({ path: "../.env" });
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
